a3756b7d317ac5394993e2431db3720b9de28047
achira-sriwattana <146061834+achira-sriwattana@users.noreply.github.com>
2024-07-06 07:51:22 +0000

    created stack node code

---
+++ Node_stack.h
@@ -0,0 +1,51 @@
+#include <string>
+#ifndef node_h
+#define node_h
+
+class NODE{
+    private:
+        string data_string; // food name
+        int data_int; // calories
+        NODE *nextPtr;
+    public:
+        NODE(string, int);
+        ~NODE();
+        void set_next(NODE*);
+        void print();
+        NODE* get_next();
+        int get_data_int();
+        string get_data_string();
+};
+
+typedef NODE* NodePtr_stack;
+
+NODE::NODE(string a, int b){
+    data_string = a;
+    data_int = b;
+    nextPtr = NULL;
+}
+NODE* NODE::get_next(){
+    return nextPtr;
+}
+
+int NODE::get_data_int(){
+    return data_int;
+}
+
+string NODE::get_data_string(){
+    return data_string;
+}
+
+void NODE::set_next(NODE *t){
+    nextPtr = t;
+}
+void NODE::print(){
+    cout << data_string << ", " << data_int << " kcal" << endl;
+}
+
+NODE::~NODE(){
+    cout<<"deleting "<< data_string << ", " << data_int << endl;
+}
+
+
+#endif
