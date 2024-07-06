01df8147807088a14991632353b4a1f06707fa2e
NongBossie <167951189+NongBossie@users.noreply.github.com>
2024-07-06 16:20:33 +0000

    bb

---
+++ sorting.h
@@ -0,0 +1,29 @@
+void display(int a[],int N){
+        int i;
+       for(i=0;i<N;i++)
+           cout<<a[i]<<" ";
+       cout<<endl;
+    
+}
+
+void swap(int & x,int &y)
+{
+        int temp=y;
+        y=x;
+        x=temp;
+    
+}
+void bubbleSort(int a[],int N)
+{
+    int temp, i, j;
+    for(i = 0; i < N-1; i++)  
+    {
+        for(j = 0; j < N-i-1; j++) 
+        {
+            if(a[j] > a[j+1])
+            {
+                swap(a[j], a[j+1]);
+            }
+        }
+    }
+}
