c7801e278c892c0c81143acb743a659643bc95e1
boonluar <167950561+boonluar@users.noreply.github.com>
2024-07-08 15:49:06 +0000

    [Add] features menu and cin

--- main.cpp
+++ main.cpp
@@ -1,24 +1,61 @@
 #include <iostream>
 #include <string>
 #include <iostream>
+#include <fstream>
 #include "ll.h"
 #include "stack.h"
 using namespace std;
 
-int main (int argc, char* argv[])
+void menu(const std::string& filename) {
+    std::ifstream inputFile(filename); // Open the file
+
+    // Check if the file was opened successfully
+    if (!inputFile) {
+        std::cerr << "Unable to open file " << filename << std::endl;
+        return; // Exit the function
+    }
+
+    std::string line;
+    // Read the file line by line and print to the console
+    while (std::getline(inputFile, line)) {
+        std::cout << line << std::endl;
+    }
+
+    inputFile.close(); // Close the file
+}
+
+
+int main ()
 {
-    Stack food_stack;
-    food_stack.push("Pad Thai", 800);
+    string name;
+    char gender;
+    int weight, age;
+    std::string filename = "menu1.txt";
+    cout << "Menu" << endl;
+    menu(filename);
+    cout << endl;
 
-    LL list;
-    LL au;
-    au.insert("au",'F', 30,65);
-    list.insert("Alice", 'F', 30, 65);
-    list.insert("Bob", 'M', 25, 80);
-    list.insert("Charlie", 'M', 35, 70);
+    LL person;
+    // Stack food_stack;
+    // food_stack.push("Pad Thai", 800);
+    for (int i = 0; i < 3;i++){
+ 
+        cout << "name" << endl;
+        cin >> name;
+        cout << "gender" << endl;
+        cin >> gender;
+        cout << "weight" << endl;
+        cin >> weight;
+        cout << "age" << endl;
+        cin >> age;
+        cout << endl;
+        person.insert(name,gender,age,weight);
+    }
+    person.print_list();
 
-    list.print_list();
+
 
 
     return 0;
 }
+
