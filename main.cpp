e5c89c22748ea39e29f18d68371416114759f45e
NongName <167950869+NongName@users.noreply.github.com>
2024-07-11 14:47:28 +0000

    done

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@
 #include "ll.h"
 #include "stack.h"
 #include "sorting.h"
-
 #include <iomanip>
+#include <stack>
+
 using namespace std;
 
 
@@ -17,6 +18,43 @@ void displayMenu(const vector<string>& menuItems) {
     for (size_t i = 0; i < menuItems.size(); ++i) {
         cout  << menuItems[i] << endl;
     }
+}
+
+void fat()
+{
+cout<<"  @@ @#  # @ @@#@@@@@@@@@@@@@@@    @@ @@  @#  @  @ #@# @ @  @@@@@@@@@@@@@@@@  &@@";cout<<endl;
+cout<<"@@@@@@@@@@@@/@@@@@@@@@@@@@@@@@@     &@@@@@@@@@@@@@@@@@@@@#@@@@@@@@@@@@@@@@@@    ";cout<<endl;
+cout<<"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@@@@@@@@@@@@@@@@&&,&&@@@@@@@@@@@@@@@@@@&%,,,%&@@@@@@@@@@@@@@@@@@@@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@@@@@@@@@@@@@@@&,,,%,,,&@&&&&&&&&&&&&*,,&*(,,&@@@@@@@@@@@@@@@@@@@@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@@@@@@@@@@@@@@@&,,,,,,,,,,,,,,,, ,,,,,,,,,,,,&&@@@@@@@@@@@@@@@@@@@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@@@@@@@@@@@@@@@&,(*,,,,,,,,,  ,,  ,,,,,,,,,,,,&&@@@@@@@@@@@@@@@@@@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@@   @  @@@@/@&(,,,,,,,,*,,         &.%&,,,,,*,,,&  @@ @ @@@@@ @@@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@  , @  @@, @%*,,*,,,,&/%%&         &&&/,,,,,*,,,,,%@@@ @  @     /@   ,/ ";cout<<endl;
+cout<<"@@@@@@@@@@@ /@@@@@&&*,,,,,,,,                    *,,,/*,,* &@@@@@@@@@@@@@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@@@@@@@@@@@&,,,**,,,      %.  .*,&,,*%(    ,,,,,,,,  &@@@@@@@@@@@@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@@@@@@@@@@&&. ,,,,            .,,,             *     &&&@@@@@@@@@@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@@@@@@@@&//*,                                      ,,,,,&&@@@@@@@@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@@@@@@&&/*****      ,,                  ,,       .,,,,,,,#&@@@@@@@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@@@@@&//*********      .,,,,,,,,,,,,,,.        ,,,,  ,,,,,*&@@@@@@@@@@   ";cout<<endl;
+cout<<"  / @  @ & %%//*******,                 ..    .       ****,*,,,,,,&&@@@@@@@  ,@@";cout<<endl;
+cout<<"@@@@@@@@@@@&//**********&,,       .               .,/,***,*,,,,,,,,&@@@@@@@  @@"; cout<<endl;
+cout<<"@@@@@@@@@@&////************&,                   ,&,,,,,,,,,,,*,,,,,,&@@@@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@@&//////(/************,                 ,&,,,,,,,,/(/,,,,,,,&&@@@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@&&////////%//******/*&,                 ,/&/&//&&//,,,,,,  ,*&@@@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@&***,,,/////,*&(//(&&,,                   ,,,,,.           //&@@@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@&&(/,,,,,,,,,,,,,,,,,.......       ..      ......     *****/&&@@@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@&&////*,,,,,,,................................     ********/&@@@@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@ &**,*,*,,,,,,... . .........................  . . ..... .,,& @  @  @ @ ";cout<<endl;
+cout<<"@@@@@@@@@&***,,,,,,,,,,,..............................    ........,&/*&&@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@@@&(((,,,,,,,,,,,,....................................***/&&**,*&@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@@@@&(((////,,,,,,,,,,,..................,,,,,,,,,,,,///((&((,**,&@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@@@@@&&***,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,**&&*((/**&@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@@@@@@@&&****,,#&&&,,,,,,,,,,,,,,,,,,,,,,,,**&&&&/****&&#*/#(&&@@@@@@@@@@";cout<<endl;
+cout<<"@@@@@@@@@@@@@@@@@@&&((((####&****/*,,,,,,,,,*****%####&#&#&&&&&&&&@@@@@@@@@@@@@@";cout<<endl;
+cout<<"@@@ @@@@@@@@@@@@@@@@@@@&&&&@@@@ %    @@ @,@@@@@ @@@@@@@@@@@@@@@@@@@@@@@@@@@ & @@";cout<<endl;
+cout<<"* @    @ (  @ @@@@@@@@@@@@@@@@%   &  @@@ @  @ /. . @    @ @@@@@@@@@@@@@@@@@  @  ";cout<<endl;
+
 }
 
 vector<string> loadMenu(const string& filename) {
@@ -52,83 +90,65 @@ double calculateBMR(char gender, double weight, double height, int age) {
      return bmr;
 } 
 
-void choices(int a, const vector<string>& menuItems,const vector<string>& calItems, Stack **food){
+void choices(int a, const vector<string>& menuItems, const vector<string>& calItems, Stack* food) {
     if (a > 0 && a <= menuItems.size()) {
-            (*food)->push(menuItems[a - 1], calItems[a-1]);
-        } else {
-            cout << "Invalid choice, skipping food selection." << endl;
-        }
+        food->push(menuItems[a - 1], calItems[a - 1]);
+    } else {
+        cout << "Invalid choice, skipping food selection." << endl;
+    }
 }
 
 int main() {
     struct PersonBMR {
         string name;
-        double bmr_difference;
+        double bmr_differences;
     };
 
-    int amount_user = 3; // amount of users
-    string name[amount_user];
-    string name_one;
-    string name_two;
-    string name_three;
+    int amount_user;
+    cout << "amount user : ";
+    cin >> amount_user;
+    cout << endl;
 
+    string name[amount_user];
     char gender;
     int age;
-    double subracted_bmr_1 = 0;
-    double subracted_bmr_2 = 0;
-    double subracted_bmr_3 = 0;
-    double subtracted_bmr_temp = 0; // for display purposes
+    int bmr[amount_user];
+    double subtracted_bmr[amount_user];
+    double bmr_differences[amount_user];
     double weight, height;
-    
+
     string filename = "menu1.txt";
-    string cal = "menu.txt";
     string menuonly = "food.txt";
     string calonly = "cal.txt";
-    
-    vector<string> menuItems = loadMenu(filename); // main menu
-    vector<string> menuwithcal = loadMenu(cal); // menu with cal
-    vector<string> menu = loadMenu(menuonly); // menu use for food choice
-    vector<string> calmenu = loadMenu(calonly); // cal use for food choice
 
-    if (menuItems.empty() && menuwithcal.empty() && menu.empty() && cal.empty()) {
+    vector<string> menuItems = loadMenu(filename);
+    vector<string> menu = loadMenu(menuonly);
+    vector<string> calmenu = loadMenu(calonly);
+
+    if (menuItems.empty() && menu.empty() && calmenu.empty()) {
         return 1; // Exit if menu loading failed
     }
 
-    //displayMenu(menuItems);
-    cout << endl;
-    // displayMenu(menuwithcal);
-
     LL person;
-    Stack *foodChoices = new Stack();
-    
+    Stack* foodChoices = new Stack();
 
     for (int i = 0; i < amount_user; i++) {
         displayMenu(menuItems);
-        cout<< endl;
+        cout << endl;
 
-        cout << "Name: (user "<<i+1 << name[i] << ")" << endl;
-        for(int j=i;j< i+1;j++)
-    {
-        cout<<"Name "<<i+1<<" :"<<" ";
-        cin>>name[i];
-    }
-        //cin >> name;
+        cout << "Name: (user " << i + 1 << name[i] << ")" << endl;
+        for (int j = i; j < i + 1; j++) {
+            cout << "Name " << i + 1 << " :" << " ";
+            cin >> name[i];
+        }
 
-        /*if (i == 0) {
-            name_one = name;
-        } else if (i == 1) {
-            name_two = name;
-        } else if (i == 2) {
-            name_three = name;
-        }*/
-        do{
-        fflush(stdin);
-        cout << "Gender (M/F):" << endl;
-        cin >> gender;
-        if(gender=='m'||gender=='M'||gender=='f'||gender=='F') break;
-        }while(gender!='m'||gender!='M'||gender!='f'||gender!='F');
+        do {
+            fflush(stdin);
+            cout << "Gender (M/F):" << endl;
+            cin >> gender;
+            if (gender == 'm' || gender == 'M' || gender == 'f' || gender == 'F') break;
+        } while (gender != 'm' || gender != 'M' || gender != 'f' || gender != 'F');
 
-      
         cout << "Age:" << endl;
         cin >> age;
         cout << "Height (cm):" << endl;
@@ -136,81 +156,74 @@ int main() {
         cout << "Weight (kg):" << endl;
         cin >> weight;
         cout << endl;
+
         person.insert(name[i], gender, age, height, weight);
 
-        double bmr = calculateBMR(gender, weight, height, age);
-        cout << "BMR: " << fixed << setprecision(2)<< bmr << " calories/day" << endl;
+        bmr[i] = calculateBMR(gender, weight, height, age);
+        if(bmr[i]== -1) continue;
+        cout << "BMR: " << fixed << setprecision(2) << bmr[i] << " calories/day" << endl;
 
-        // int choice1, choice2, choice3;
         vector<int> vec(5);
         cout << "Choose 5 foods from the menu (enter the number):" << endl;
-        for (int o = 0;o<5;o++) {
+        for (int o = 0; o < 5; o++) {
             cin >> vec[o];
         }
-        
-        //output to choice func
-        for(int o : vec) {
-            choices(o,menu,calmenu,&foodChoices);
+
+        for (int o : vec) {
+            choices(o, menu, calmenu, foodChoices);
         }
-        cout <<endl;
-        double subtracted_bmr = bmr - (foodChoices->get_total_calories());
-        subtracted_bmr = subtracted_bmr *-1; // idk why subtracted_bmr is initially negative
-        // cout << "Food calories - BMR = " << subtracted_bmr << endl;
-         cout << "BMR  = " << bmr  << " - Food calories = " << foodChoices->get_total_calories() << endl;
-         cout << "Calories left = " << bmr - foodChoices->get_total_calories() <<endl;
-         
-        
+
+        cout << endl;
+        subtracted_bmr[i] = bmr[i] - (foodChoices->get_total_calories());
+        //subtracted_bmr[i] = subtracted_bmr[i] * -1;
+
+        cout << "BMR  = " << bmr[i] << " - Food calories = " << foodChoices->get_total_calories() << endl;
+        cout << "Calories left = " << bmr[i] - foodChoices->get_total_calories() << endl;
+       
+        if(bmr[i] - foodChoices->get_total_calories()<0)
+        {
+            fat();
+        }
         cout << endl << "Food choices:" << endl;
         foodChoices->printStack();
+        foodChoices->clearStack();
 
-        
-
-        // Store the result in the appropriate variable
-        if (i == 0) {
-            subracted_bmr_1 = subtracted_bmr;
-        } else if (i == 1) {
-            subracted_bmr_2 = subtracted_bmr;
-        } else if (i == 2) {
-            subracted_bmr_3 = subtracted_bmr;
+        for (int k = 0; k < amount_user; k++) {
+            cout << subtracted_bmr[k];
+            
         }
 
-        foodChoices->clearStack();
-        // if (choice > 0 && choice <= menuwithcal.size()) {
-        //     foodChoices.push(menuwithcal[choice - 0]);
-        // } else {
-        //     cout << "Invalid choice, skipping food selection." << endl;
-        // }
+        bmr_differences[amount_user];
+        double bmr_array[amount_user];
+
+        for (int j = 0; j < amount_user; ++j) {
+            bmr_differences[j] = subtracted_bmr[j]*-1;
+            if (bmr_differences[j] < 0) ; 
+            //else  cout<< "You did not eat excessively";break;
+
+        }
+        bmr_differences[i] = subtracted_bmr[i];
     }
+
     person.print_list();
-    
-    PersonBMR bmr_array[3] = {
-        {name_one, subracted_bmr_1},
-        {name_two, subracted_bmr_2},
-        {name_three, subracted_bmr_3}
-    };
 
-    // Extract BMR differences into a separate array
-    double bmr_differences[3];
+     
+     /*for(int i=0;i<amount_user;i++)
+    {
+        //PersonBMR bmr_array[i]={name[i+1], subtracted_bmr[i+1]};
+        //bmr_differences[i] = bmr_array[i].bmr_differences;
+          bmr_differences[i] = subtracted_bmr[i];
+    };*/
 
-    for (int i = 0; i < 3; ++i) {
-        bmr_differences[i] = bmr_array[i].bmr_difference;
-    }
-    
+
     bubbleSort(bmr_differences, amount_user);
-    cout << "Sorted BMR: \n";
+    cout << "Sorted BMR: "<<endl;
 
-    for (int i = 0; i < 3; i++) {
-        cout << fixed << setprecision(2);
-            
-                cout << bmr_differences[i] << endl;
-            
+    for (int i = 0; i < amount_user; i++) {
+        cout << fixed << setprecision(2) << bmr_differences[i] << endl;
     }
 
     delete foodChoices;
-    // while (!foodChoices.isEmpty()) {
-    //     cout << foodChoices.topElement() << endl;
-    //     foodChoices.pop();
-    // }
 
     return 0;
 }
